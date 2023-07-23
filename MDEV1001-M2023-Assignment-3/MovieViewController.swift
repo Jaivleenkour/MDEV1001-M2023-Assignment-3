87b8b9b847e0bd8da2bdcea90c92707253813e7f
JaivleenKour <Jaivleenkour11@gmail.com>
2023-07-23 17:04:44 -0400

    added no data available text for the first time in table background view in MovieViewController

--- MDEV1001-M2023-Assignment-3/MovieViewController.swift
+++ MDEV1001-M2023-Assignment-3/MovieViewController.swift
@@ -14,10 +14,6 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
     
     @IBOutlet weak var searchBar: UISearchBar!
     
-
-    
-   // var movies = [Array<Any>]()
-    var index = Int()
     var selectedPosterImage: UIImage?
     var check = "no"
     var apiKey = ""
@@ -29,92 +25,23 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
         apiKey = "80597972"
         
         //self.searchBar.delegate = self
-//        let noDataLabel: UILabel  = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
-//                noDataLabel.text          = "Oops!No data available.\nSearch for movies to retrieve the data."
-//        noDataLabel.numberOfLines = 0
-//                noDataLabel.textColor     = UIColor.black
-//                noDataLabel.textAlignment = .center
-//        self.tableView.backgroundView  = noDataLabel
-//
-       /* fetchMovies { [weak self] movies, error in
-            DispatchQueue.main.async
-            {
-                if let movies = movies
-                {
-                    if movies.isEmpty
-                    {
-                        // Display a message for no data
-                        self?.displayErrorMessage("No movies available.")
-                    } else {
-                        self?.movies = movies
-                        self?.tableView.reloadData()
-                    }
-                } else if let error = error {
-                    if let urlError = error as? URLError, urlError.code == .timedOut
-                    {
-                        // Handle timeout error
-                        self?.displayErrorMessage("Request timed out.")
-                    } else {
-                        // Handle other errors
-                        print("err",error.localizedDescription.codingKey)
-                        self?.displayErrorMessage(error.localizedDescription)
-                    }
-                }
-            }
-        }*/
-    }
-    /*
-    func displayErrorMessage(_ message: String)
-    {
-        print("msg",message)
-        let alertController = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
-        present(alertController, animated: true, completion: nil)
-    }
+        let noDataLabel: UILabel  = UILabel(frame: CGRect(x: 0, y: 0, width: tableView.bounds.size.width, height: tableView.bounds.size.height))
+                noDataLabel.text          = "Oops!No data available.\nSearch for movies to retrieve the data."
+        noDataLabel.numberOfLines = 0
+                noDataLabel.textColor     = UIColor.black
+                noDataLabel.textAlignment = .center
+        self.tableView.backgroundView  = noDataLabel
     
-    func fetchMovies(completion: @escaping ([Movie]?, Error?) -> Void)
-    {
-        print("urlll","https://www.omdbapi.com/?apikey=\(apiKey)&t=Mission+Impossible")
-        guard let url = URL(string: "https://www.omdbapi.com/?apikey=\(apiKey)&t=Mission+Impossible") else
-        {
-            completion(nil, nil) // Handle URL error
-            return
-        }
-
-        URLSession.shared.dataTask(with: url) { data, _, error in
-            if let error = error {
-                completion(nil, error) // Handle network error
-                return
-            }
-
-            print("data",data)
-            guard let data = data else {
-                completion(nil, nil) // Handle empty response
-                return
-            }
-
-            do {
-                print("dhgghggg",data)
-                let objMov =
-                let movies = try JSONDecoder().decode([Movie].self, from: data)
-                completion(movies, nil) // Success
-            } catch {
-                completion(nil, error) // Handle JSON decoding error
-            }
-        }.resume()
     }
-*/
+ 
     
     func fetchMovieDataFromAPI(_ searchText : String) {
         
-        
-            // Replace apiURL with the actual URL of the API endpoint
-        let newsearchText = searchText.replacingOccurrences(of: " ", with: "+")
-            print("urlll","https://www.omdbapi.com/?apikey=\(apiKey)&t=\(newsearchText)")
-        
-            guard let apiURL = URL(string: "https://www.omdbapi.com/?apikey=\(apiKey)&t=\(newsearchText)") else {
-                print("Invalid API URL")
-                return
+        // Replace apiURL with the actual URL of the API endpoint
+           let newsearchText = searchText.replacingOccurrences(of: " ", with: "+")
+           guard let apiURL = URL(string: "https://www.omdbapi.com/?apikey=\(apiKey)&t=\(newsearchText)") else {
+           print("Invalid API URL")
+           return
             }
             
             URLSession.shared.dataTask(with: apiURL) { data, response, error in
@@ -129,7 +56,6 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
                     let movie = try decoder.decode(Movie.self, from: data)
                     self.arr_movies.append(movie)
                     print("mmmm",self.arr_movies.count)
-                   // self.checkforTV()
                     // Now you have the movie object, you can use it as needed
                     print(movie.Title)
                     print(movie.Genre)
@@ -138,38 +64,22 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
                         self.searchBar.text = ""
                         self.tableView.reloadData()
                     }
-                    // ... and so on
-                    
                     
                 } catch {
                     print("Error parsing JSON: \(error)")
                 }
             }.resume()
         
-        
 }
 
- func checkforTV()
-    {
-        if self.arr_movies.count > 0
-        {
-            //tableView.reloadData()
-        }
-        
-    }
 
-
-    
     @IBAction func searchButton(_ sender: Any) {
         self.view.endEditing(true)
-
         if self.searchBar.text != ""
         {
-//            check = "yes"
-//            self.tableView.backgroundView = nil
+            check = "yes"
+            self.tableView.backgroundView = nil
             fetchMovieDataFromAPI(self.searchBar.text!)
-            
-            
             
         }
         else{
@@ -179,13 +89,10 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
         }
     }
     
-    
-    
+   
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        print("cc",self.arr_movies.count)
         if self.arr_movies.count > 0{
-            print("c2",self.arr_movies.count)
             return self.arr_movies.count
         }else{
             print("c3",self.arr_movies.count)
@@ -210,7 +117,6 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieTableViewCell
         
         let movie = arr_movies[indexPath.row]
-   
         cell.titleLabel?.text = movie.Title
         cell.studioLabel?.text = "Studio: \(movie.Production)"
         cell.ratingLabel?.text = "\(movie.imdbRating)"
@@ -218,9 +124,7 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
         
         
         let url = URL(string: movie.Poster)!
-
-            // Fetch Image Data
-        
+        // Fetch Image Data
         DispatchQueue.global().async {
                 // Fetch Image Data
                 if let data = try? Data(contentsOf: url) {
@@ -230,15 +134,7 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
                     }
                 }
             }
-        
- 
-
-
-
-        //            let imgData = movie.thumbnail != nil ? UIImage(data: movie.thumbnail!) : nil
-        //            cell.thumbnailImage?.image = imgData ?? selectedPosterImage
         return cell
-        
         
     }
     
@@ -249,7 +145,5 @@ class MovieViewController: UIViewController , UITableViewDelegate, UITableViewDa
         performSegue(withIdentifier: "MovieDetailSegue", sender: indexPath)
     }
    
-   
-    
    
 }
